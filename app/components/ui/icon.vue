50fb21561360671db6297f13d05ddc5a6ecfd68f
Chris <gilges.dominik@gmail.com>
2026-08-03 22:11:40 +0200

    feat: add tabler icons to app

---
+++ app/components/ui/icon.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Icon as NuxtIcon } from '#components';
+
+withDefaults(defineProps<{
+  name: keyof typeof ICONS;
+  size?: number;
+}>(), { size: 16 });
+
+const ICONS = {
+  plus: 'tabler:plus',
+  play: 'tabler:player-play',
+  pause: 'tabler:player-pause',
+  check: 'tabler:check',
+  more: 'tabler:dots-vertical',
+  grip: 'tabler:grip-vertical',
+  pin: 'tabler:pin',
+  share: 'tabler:share',
+  trophy: 'tabler:trophy',
+  eye: 'tabler:eye',
+  copy: 'tabler:copy',
+  chevron: 'tabler:chevron-down',
+  close: 'tabler:x',
+  minus: 'tabler:minus',
+  edit: 'tabler:pencil',
+  trash: 'tabler:trash',
+  duplicate: 'tabler:copy-plus',
+  reset: 'tabler:rotate',
+} as const;
+</script>
+
+<template>
+  <NuxtIcon
+    :name="ICONS[name]"
+    :size="size"
+  />
+</template>
