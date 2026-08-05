17ee2243640dbe164f6ab1941d55c2d297495a7c
Chris <gilges.dominik@gmail.com>
2026-08-05 19:48:38 +0200

    feat(client): add reusable modal component

---
+++ app/components/ui/modal.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  modalId: string;
+  modalTitle: string;
+
+}>();
+</script>
+
+<template>
+  <dialog :id="modalId" class="modal">
+    <div class="modal-box border border-neutral rounded-2xl bg-base-200">
+      <div class="flex flex-col gap-4">
+        <h3 class="text-lg font-bold">
+          {{ props.modalTitle }}
+        </h3>
+        <slot />
+      </div>
+      <div class="modal-action">
+        <form method="dialog">
+          <div class="flex gap-2">
+            <button class="btn rounded-lg border border-neutral">
+              Cancel
+            </button>
+            <button class="btn btn-primary rounded-lg border border-neutral text-neutral">
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </dialog>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Wintool Starter</h1>
+  <div>
+    <!--  modal testzone -->
+    <div>
+      <button class="btn" onclick="test_modal.showModal()">
+        Modal hier testen
+      </button>
+      <Modal
+        modal-id="test_modal"
+        modal-title="New Winnie"
+      >
+        <input
+          type="text"
+          placeholder="Name of your Winnie"
+          class="input  bg-base-300"
+        >
+      </Modal>
+    </div>
+  </div>
 </template>
