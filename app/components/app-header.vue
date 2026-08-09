da451ff4f11ecb21f1c111c9a57961118360b0b7
Chris <gilges.dominik@gmail.com>
2026-08-09 12:40:57 +0200

    add layouts for owner and shared view

---
+++ app/components/app-header.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header class="flex flex-wrap items-center gap-2">
+    <span class="type-wordmark">Winnies</span>
+    <span class="select">Run Picker</span>
+    <UiIconButton
+      icon="plus"
+      label="add new winnie"
+    />
+    <UiIconButton
+      icon="share"
+      label="open shared view"
+    />
+    <div class="ms-auto" />
+    <UiIconButton
+      icon="eye"
+      label="login?"
+    />
+  </header>
+</template>
