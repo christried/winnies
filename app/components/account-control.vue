63c0b8e4300d902ee0ff3f4dea1ea5dcbe030aea
Chris <gilges.dominik@gmail.com>
2026-08-16 20:04:53 +0200

    feat(client): add account-control to header and make discord login reachable

---
+++ app/components/account-control.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup>
+const { data: session } = await useAuth();
+const signingIn = ref(false);
+
+const userShorthand = computed(() => {
+  return session.value?.user.name.slice(0, 2).toUpperCase();
+});
+
+/**
+ * Handles clicks on the sign in button at the top right and navigates to providers defined in authClient.
+ */
+async function onSignIn() {
+  signingIn.value = true;
+  await authClient.signIn.social({ provider: "discord", callbackURL: "/" });
+}
+
+/**
+ * Processes logging out the current User.
+ * @param close Passed up from the menu. Gives access to closing the popover menu.
+ */
+async function onSignOut(close: () => void) {
+  close();
+  await authClient.signOut();
+  await navigateTo("/", { replace: true });
+}
+</script>
+
+<template>
+  <button
+    v-if="!session"
+    type="button"
+    class="btn btn-neutral"
+    :disabled="signingIn"
+    @click="onSignIn()"
+  >
+    <span v-if="signingIn" class="loading loading-md loading-dots" />
+    Sign in with Discord
+  </button>
+
+  <UiDropdown
+    v-else
+    label="Account Menu"
+    menu-class="dropdown-end"
+  >
+    <template #trigger>
+      <div v-if="session.user.image" class="avatar">
+        <div class="w-12 rounded-full">
+          <img alt="" :src="session.user.image">
+        </div>
+      </div>
+      <div v-else class="avatar avatar-placeholder">
+        <div class="w-12 rounded-full bg-neutral text-neutral-content">
+          <span> {{ userShorthand }}</span>
+        </div>
+      </div>
+    </template>
+    <template #default=" { close }">
+      <li class="menu-title">
+        Logged in as {{ session.user.name }}
+      </li>
+      <li>
+        <button type="button" @click="onSignOut(close)">
+          Log out
+        </button>
+      </li>
+    </template>
+  </UiDropdown>
+</template>
