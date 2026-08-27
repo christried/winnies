670d7a621912eeced19bc3219ceb090bfb845252
Chris <gilges.dominik@gmail.com>
2026-08-27 19:21:55 +0200

    feat(client): first addition of session guards

--- app/components/app-header.vue
+++ app/components/app-header.vue
@@ -1,19 +1,21 @@
 <script lang="ts" setup>
-
+const { data: session } = await useAuth();
 </script>
 
 <template>
   <header class="flex flex-wrap items-center gap-2">
     <span class="type-wordmark">Winnies</span>
-    <span class="select">Winnie Picker</span>
-    <UiIconButton
-      icon="plus"
-      label="add new winnie"
-    />
-    <UiIconButton
-      icon="share"
-      label="open shared view"
-    />
+    <template v-if="session">
+      <span class="select">Winnie Picker</span>
+      <UiIconButton
+        icon="plus"
+        label="add new winnie"
+      />
+      <UiIconButton
+        icon="share"
+        label="open shared view"
+      />
+    </template>
     <div class="ms-auto" />
     <AccountControl />
   </header>
