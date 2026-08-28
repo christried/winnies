34c2163fb14d3f3a3f3f87fc2b88d9df2cb0d6cc
Chris <gilges.dominik@gmail.com>
2026-08-28 19:44:56 +0200

    feat(client): first version of new winnie modal

---
+++ app/components/new-winnie-modal.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineEmits<{
+  /**
+   * dasd
+   */
+  created: [];
+}>();
+
+const modal = useTemplateRef("modal");
+
+defineExpose({
+  open: () => modal.value?.open(),
+  close: () => modal.value?.close(),
+});
+
+/**
+ * dasda
+ */
+function onSubmit() {}
+</script>
+
+<template>
+  <UiModal
+    ref="modal"
+    title="New Winnie"
+    action-label="Create Winnie"
+    @action="onSubmit"
+  >
+    <form @submit="onSubmit">
+      <div class="input" />
+    </form>
+  </UiModal>
+</template>
