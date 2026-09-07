0f2c1c032cbc8b5de9158334d2016b2370f228b8
Chris <gilges.dominik@gmail.com>
2026-09-07 19:10:35 +0200

    feat(client): add proper footer to app incl. styling

--- app/components/app-wordmark.vue
+++ app/components/app-wordmark.vue
@@ -1,4 +1,11 @@
 <script lang="ts" setup>
+defineProps<{
+  /**
+   * Footer Component only wants the global wordmark
+   */
+  isFooter?: boolean;
+}>();
+
 const runtimeConfig = useRuntimeConfig();
 </script>
 
@@ -6,18 +13,21 @@ const runtimeConfig = useRuntimeConfig();
   <div class="flex items-center justify-center gap-2">
     <span class="type-wordmark">Winnies</span>
 
-    <div class="badge badge-outline badge-warning">
-      v{{ runtimeConfig.public.appVersion }} · beta
-    </div>
-
-    <NuxtLink
-      to="https://github.com/christried/winnies/issues/new/choose"
-      target="_blank"
-      rel="noopener"
-    >
-      <div class="tooltip tooltip-bottom tooltip-warning" data-tip="Report an issue">
-        <UiIcon name="bug" :size="24" />
+    <template v-if="!isFooter">
+      <div class="badge badge-outline badge-warning">
+        v{{ runtimeConfig.public.appVersion }} · beta
       </div>
-    </NuxtLink>
+
+      <NuxtLink
+        to="https://github.com/christried/winnies/issues/new/choose"
+        target="_blank"
+        rel="noopener"
+        external
+      >
+        <div class="tooltip tooltip-bottom tooltip-warning" data-tip="Report an issue">
+          <UiIcon name="bug" :size="24" />
+        </div>
+      </NuxtLink>
+    </template>
   </div>
 </template>
